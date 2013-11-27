33c6ec6588adcfbce49453159a433bbe2de16946
huangxin03 <huangxin03@baidu.com>
2013-11-27 20:31:21 +0800

    index + 1

--- examples/index.js
+++ examples/index.js
@@ -1,10 +1,23 @@
+function showcase(name){
+	
+	var list = document.getElementById("demoList");
+	list.style.webkitTransition = "all 0.2s ease-out";
+	list.style.right = list.clientWidth + "px";
+	
+	var href = name + "/" + name + ".html";
+	setTimeout(function(){
+		location.href = href;
+	}, 250);
+	
+}
+
 touch.on(document, 'DOMContentLoaded', function(e){
 	
-	var on = touch.on;
-	
-	on("#demoList", 'tap', 'li', function(e){
-		var href = this.getAttribute("data-href");
-		if(href){ location.href = href; }
+	var demoList = document.getElementById("demoList");
+	demoList.classList.add("show");
+
+	touch.on(demoList, 'tap', 'li', function(e){
+		var name = this.getAttribute("data-name");
+		name && showcase(name);
 	});
-	
 });
