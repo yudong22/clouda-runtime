3e59e1dd18d0ca1b0cddffb7f5f476c1e1d72fc5
sundong <sundong@baidu.com>
2013-12-16 17:54:03 +0800

    提交lbs

--- src/mbaas/map.js
+++ src/mbaas/map.js
@@ -1,6 +1,6 @@
 define("mbaas",function(module) {
     var lightapp = this;
-    //定义 battery 空间，clouda.device.battery 支持退化
+    //定义 map 空间，clouda.mbaas.map 
     var it = module.map = {};
     
     /**
@@ -9,6 +9,89 @@ define("mbaas",function(module) {
      * @instance
      * @namespace clouda.mbaas.map
      */
+    var start = new delegateClass("map","start");
+    var stop = new delegateClass("map","stop");
+    var locationRequest = new delegateClass("map","locationRequest");
+    var poiRequest = new delegateClass("map","poiRequest");
     
+    var loadScript = function(xyUrl,callback){
+        var head = document.getElementsByTagName('head')[0];
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = xyUrl;
+        //借鉴了jQuery的script跨域方法
+        script.onload = script.onreadystatechange = function() {
+            if ((!this.readyState || this.readyState === "loaded" || this.readyState === "complete")) {
+                if (typeof callback === 'function') {
+                    callback();
+                }
+                // Handle memory leak in IE
+                script.onload = script.onreadystatechange = null;
+                if (head && script.parentNode) {
+                    head.removeChild(script);
+                }
+            }
+        };
+        head.insertBefore(script, head.firstChild);
+    };
+    
+    it.Convertor = {};
+    
+    it.Convertor.translate = function(point, type, callback) {
+        var callbackName = 'cbk_' + Math.round(Math.random() * 10000); //随机函数名
+        var xyUrl = "http://api.map.baidu.com/ag/coord/convert?from=" + type + "&to=4&x=" + point.lng + "&y=" + point.lat + "&callback=clouda.mbaas.map.Convertor." + callbackName;
+        //动态创建script标签
+        loadScript(xyUrl);
+        it.Convertor[callbackName] = function(xyResult) {
+            delete it.Convertor[callbackName]; //调用完需要删除改函数
+            var point = new BMap.Point(xyResult.x, xyResult.y);
+            if (typeof callback === 'function') {
+                callback(point);
+            }
+        };
+    };
+    
+    it.start = function(options){
+        
+        start(function(data){
+            //{lng,lat}
+            var gpsPoint = new BMap.Point(data.longitude, data.latitude);
+            it.Convertor.translate(gpsPoint, 2,
+            function(point) {
+                options.onsuccess(point);
+           
+            }); //真实经纬度转成百度坐标
+            
+        },function(nativeErr){
+            lightapp.error(ErrCode.MAP_ERROR,nativeErr,options);
+        },options);
+    };
+    
+    
+    it.stop = function(options){
+        stop(function(data){
+            options.onsuccess(clouda.STATUS.SUCCESS);
+        },function(nativeErr){
+            lightapp.error(ErrCode.MAP_ERROR,nativeErr,options);
+        },options);
+    };
+    
+    it.locationRequest = function(options){
+        //{longitude,latitude,loctype:161}
+        locationRequest(function(data){
+            options.onsuccess(data);
+        },function(nativeErr){
+            lightapp.error(ErrCode.MAP_ERROR,nativeErr,options);
+        },options);
+    };
+    
+    it.poiRequest = function(options){
+        //{poi:{p:[{x,y,dis,name}]}}
+        poiRequest(function(data){
+            options.onsuccess(data);
+        },function(nativeErr){
+            lightapp.error(ErrCode.MAP_ERROR,nativeErr,options);
+        },options);
+    };
     
 });
