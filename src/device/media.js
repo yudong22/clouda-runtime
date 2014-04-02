9b9bd2c37c18ccc8e34e49e50d947dfab2f1a2a7
sundong <sundong@baidu.com>
2014-04-02 13:52:39 +0800

    update media speedFF

--- src/device/media.js
+++ src/device/media.js
@@ -365,8 +365,7 @@ var getPicture = new delegateClass("device","camera","getPicture");
                     // console.log(JSON.stringify(media[link].getCurrentPosition()));//getCurrentPosition
                     // var duration = media[link][operator]();
                     //clouda.STATUS.SUCCESS
-                    media[link][operator](5000);
-                    options.onsuccess(clouda.STATUS.SUCCESS);
+                    media[link][operator](5000,options);
                     break;
                 case "play"://应该添加完成的callback
                     // media[link][operator](options.onsuccess);
